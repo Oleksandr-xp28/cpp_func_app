f7373fb8edd07762210380c82d7a90906aa42cf9
Oleksandr <fedorut20@gmail.com>
2023-06-07 14:29:09 +0300

    task 3 done

--- main_func.cpp
+++ main_func.cpp
@@ -4,10 +4,41 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+void findPerfectNumbers(int start, int end) {
+    int perfectNumbers[100];
+    int count = 0;
+    for (int n = start; n <= end; n++) {
+        int sum = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum == n) {
+            perfectNumbers[count] = n;
+            count++;
+        }
+    }
+
+    cout << "The perfect numbers in the interval [" << start << ", " << end << "] are: ";
+    for (int i = 0; i < count; i++) {
+        cout << perfectNumbers[i] << " ";
+    }
+    cout << endl;
 }
+
+int main() {
+    int start, end;
+    cout << "Enter the start of the interval: ";
+    cin >> start;
+    cout << "Enter the end of the interval: ";
+    cin >> end;
+
+    findPerfectNumbers(start, end);
+
+    return 0;
+}
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
