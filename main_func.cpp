bbed7aa1d65f175cacd5892b3a8d6474d3e0c2bc
Oleksandr <fedorut20@gmail.com>
2023-06-07 14:13:06 +0300

    task 5 done

--- main_func.cpp
+++ main_func.cpp
@@ -1,12 +1,34 @@
 // cpp_func_app.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+bool isLucky(int sd) {
+    int firstThree = 0, lastThree = 0;
+    for (int i = 0; i < 3; i++) {
+        lastThree += sd % 10;
+        sd /= 10;
+    }
+    for (int i = 0; i < 3; i++) {
+        firstThree += sd % 10;
+        sd /= 10;
+    }
+    return firstThree == lastThree;
+}
+
+int main() {
+    int sd;
+    cout << "Enter a six-digit number: ";
+    cin >> sd;
+    if (isLucky(sd)) {
+        cout << sd << " is a lucky number!" << endl;
+    }
+    else {
+        cout << sd << " is not a lucky number." << endl;
+    }
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
