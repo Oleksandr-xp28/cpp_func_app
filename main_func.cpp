2881fe026c6bcee66286f0727e00fec0dfbb38d9
Oleksandr <fedorut20@gmail.com>
2023-06-07 13:48:16 +0300

    Add project files.

---
+++ main_func.cpp
@@ -0,0 +1,14 @@
+// cpp_func_app.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+// In the future, to open this project again, go to File > Open > Project and select the .sln file
