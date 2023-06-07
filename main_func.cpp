0fe7dedd9e40584d816258cfb09206c1694d5b9e
Oleksandr <fedorut20@gmail.com>
2023-06-07 14:34:33 +0300

    task 1 done

--- main_func.cpp
+++ main_func.cpp
@@ -4,10 +4,27 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int power(int base, int exponent) {
+    int result = 1;
+    for (int i = 0; i < exponent; i++) {
+        result *= base;
+    }
+    return result;
 }
+
+int main() {
+    int base, exponent;
+    cout << "Enter the base: ";
+    cin >> base;
+    cout << "Enter the exponent: ";
+    cin >> exponent;
+
+    int result = power(base, exponent);
+    cout << base << " raised to the power of " << exponent << " is: " << result << endl;
+
+    return 0;
+}
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
