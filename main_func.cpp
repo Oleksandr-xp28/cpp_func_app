7c80d17d9be9c1784e2a7f14b7923d2c10c6c952
Oleksandr <fedorut20@gmail.com>
2023-06-07 14:25:12 +0300

    task 4 done

--- main_func.cpp
+++ main_func.cpp
@@ -2,11 +2,54 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+void displayCard(string card) {
+    string rank = card.substr(0, 1);
+    string suit = card.substr(1, 1);
+
+    switch (rank[0]) {
+    case 'A':
+        cout << "Ace";
+        break;
+    case 'K':
+        cout << "King";
+        break;
+    case 'Q':
+        cout << "Queen";
+        break;
+    case 'J':
+        cout << "Jack";
+        break;
+    default:
+        cout << rank;
+    }
+
+    cout << " of ";
+
+    switch (suit[0]) {
+    case 'H':
+        cout << "Hearts";
+        break;
+    case 'D':
+        cout << "Diamonds";
+        break;
+    case 'C':
+        cout << "Clubs";
+        break;
+    case 'S':
+        cout << "Spades";
+        break;
+    }
+}
+
+int main() {
+    string card;
+    cout << "Enter the first capital letter of the card: ";
+    cin >> card;
+    displayCard(card);
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
