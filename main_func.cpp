05afb23a453144190aee124b19df027175544c55
Oleksandr <fedorut20@gmail.com>
2023-06-07 14:32:23 +0300

    task 2 done

--- main_func.cpp
+++ main_func.cpp
@@ -4,10 +4,27 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int sumRange(int start, int end) {
+    int sum = 0;
+    for (int i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 }
+
+int main() {
+    int start, end;
+    cout << "Enter the start of the range: ";
+    cin >> start;
+    cout << "Enter the end of the range: ";
+    cin >> end;
+
+    int result = sumRange(start, end);
+    cout << "The sum of all numbers between " << start << " and " << end << " is: " << result << endl;
+
+    return 0;
+}
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
